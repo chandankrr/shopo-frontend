77f36b291e154ec33d1eda83e8d153b6e67f507f
Chandan Kumar <chandankrr.91@gmail.com>
2023-09-10 00:28:31 +0530

    designed register page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { LoginPage } from './Routes.js';
+import { LoginPage, SignupPage } from './Routes.js';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
