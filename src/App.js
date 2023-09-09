df8864077a72476b3e7e2a73733dd85717f6f838
Chandan Kumar <chandankrr.91@gmail.com>
2023-09-09 23:52:53 +0530

    designed login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { LoginPage } from './Routes.js';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
