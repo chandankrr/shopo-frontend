c49e33270ca3d4709cdd682640c90ac255f3349f
Chandan Kumar <chandankrr.91@gmail.com>
2023-10-07 12:20:26 +0530

    created product details page

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
               element={<ActivationPage />}
             />
             <Route path="/products" element={<ProductsPage />} />
-            <Route path="/product/:id" element={<ProductDetailsPage />} />
+            <Route path="/product/:name" element={<ProductDetailsPage />} />
             <Route path="/best-selling" element={<BestSellingPage />} />
             <Route path="/events" element={<EventsPage />} />
             <Route path="/faq" element={<FAQPage />} />
