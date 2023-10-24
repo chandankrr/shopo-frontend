753c9ccaf238167d83472ed92553e05b49a2e4dc
Chandan Kumar <chandankrr.91@gmail.com>
2023-10-24 23:39:58 +0530

    integrated paypal payment method

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -1,3 +1,4 @@
+import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
 import {
   CardCvcElement,
   CardExpiryElement,
@@ -7,6 +8,7 @@ import {
 } from '@stripe/react-stripe-js';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { RxCross1 } from 'react-icons/rx';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -26,7 +28,27 @@ const Payment = () => {
     setOrderData(orderData);
   }, []);
 
-  const createOrder = (data, actions) => {};
+  const createOrder = (data, actions) => {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            description: 'Sunflower',
+            amount: {
+              currency_code: 'USD',
+              value: orderData?.totalPrice,
+            },
+          },
+        ],
+        // not needed if a shipping address is actually needed
+        application_context: {
+          shipping_preference: 'NO_SHIPPING',
+        },
+      })
+      .then((orderID) => {
+        return orderID;
+      });
+  };
 
   const order = {
     cart: orderData?.cart,
@@ -35,9 +57,42 @@ const Payment = () => {
     totalPrice: orderData?.totalPrice,
   };
 
-  const onApprove = async (data, actions) => {};
+  const onApprove = async (data, actions) => {
+    return actions.order.capture().then(function (details) {
+      const { payer } = details;
 
-  const paypalPaymentHandler = async (paymentInfo) => {};
+      let paymentInfo = payer;
+
+      if (paymentInfo !== undefined) {
+        paypalPaymentHandler(paymentInfo);
+      }
+    });
+  };
+
+  const paypalPaymentHandler = async (paymentInfo) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    order.paymentInfo = {
+      id: paymentInfo.payer_id,
+      status: 'succeeded',
+      type: 'Paypal',
+    };
+
+    await axios
+      .post(`${server}/order/create-order`, order, config)
+      .then((res) => {
+        setOpen(false);
+        navigate('/order/success');
+        toast.success('Order successful!');
+        localStorage.setItem('cartItems', JSON.stringify([]));
+        localStorage.setItem('latestOrder', JSON.stringify([]));
+        window.location.reload();
+      });
+  };
 
   const paymentData = { amount: Math.round(orderData?.totalPrice * 100) };
 
@@ -258,22 +313,40 @@ const PaymentInfo = ({
           </h4>
         </div>
 
-        {/* pay with card */}
+        {/* pay with paypal */}
         {select === 2 ? (
           <div className="w-full flex border-b">
-            <form className="w-full" onSubmit={paymentHandler}>
-              <div className="w-full flex pb-3">
-                <div className="w-full">
-                  <label className="block pb-2">Paypal Email</label>
-                  <input required className={`${styles.input}`} />
+            <div
+              className={`${styles.button} !bg-[#f63b60] text-white h-[45px] rounded-[5px] cursor-pointer text-[18px] font-[600]`}
+              onClick={() => setOpen(true)}
+            >
+              Pay Now
+            </div>
+            {open && (
+              <div className="w-full fixed top-0 left-0 bg-[#00000039] h-screen flex items-center justify-center z-[99999]">
+                <div className="w-full 800px:w-[40%] h-screen 800px:h-[80vh] bg-white rounded-[5px] shadow flex flex-col justify-center p-8 relative overflow-y-scroll">
+                  <div className="w-full flex justify-end p-3">
+                    <RxCross1
+                      size={30}
+                      className="cursor-pointer absolute top-3 right-3"
+                      onClick={() => setOpen(false)}
+                    />
+                  </div>
+                  <PayPalScriptProvider
+                    options={{
+                      'client-id':
+                        'AfjWO9oNtKocFsqOuN0C5T_IzdyScEG4U-T656Y5h7WjmuUo04phXCa5GjhzsG7H3-47fw-aD5yrh_ti',
+                    }}
+                  >
+                    <PayPalButtons
+                      style={{ layout: 'vertical' }}
+                      onApprove={onApprove}
+                      createOrder={createOrder}
+                    />
+                  </PayPalScriptProvider>
                 </div>
               </div>
-              <input
-                type="submit"
-                value="Submit"
-                className={`${styles.button} !bg-[#f63b60] text-[#fff] h-[45px] rounded-[5px] cursor-pointer text-[18px] font-[600]`}
-              />
-            </form>
+            )}
           </div>
         ) : null}
       </div>
@@ -313,6 +386,8 @@ const PaymentInfo = ({
 };
 
 const CartData = ({ orderData }) => {
+  const shipping = orderData?.shipping?.toFixed(2);
+
   return (
     <div className="w-full bg-[#fff] rounded-md p-5 pb-8">
       <div className="flex justify-between">
@@ -322,14 +397,14 @@ const CartData = ({ orderData }) => {
       <br />
       <div className="flex justify-between">
         <h3 className="text-[16px] font-[400] text-[#000000a4]">shipping:</h3>
-        <h5 className="text-[18px] font-[600]">
-          ${orderData?.shipping?.toFixed(2)}
-        </h5>
+        <h5 className="text-[18px] font-[600]">${shipping}</h5>
       </div>
       <br />
       <div className="flex justify-between border-b pb-3">
         <h3 className="text-[16px] font-[400] text-[#000000a4]">Discount:</h3>
-        <h5 className="text-[18px] font-[600]">${orderData?.discountPrice}</h5>
+        <h5 className="text-[18px] font-[600]">
+          {orderData?.discountPrice ? '$' + orderData.discountPrice : '-'}
+        </h5>
       </div>
       <h5 className="text-[18px] font-[600] text-end pt-3">
         ${orderData?.totalPrice}
