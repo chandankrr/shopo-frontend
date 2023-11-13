6fd40524b284bd9a983013a214f95c893af7ad15
Chandan Kumar <chandankrr.91@gmail.com>
2023-11-13 22:48:02 +0530

    added running events & shop reviews in shop info page

--- src/components/Shop/ShopProfileData.jsx
+++ src/components/Shop/ShopProfileData.jsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
+import { getAllEventsShop } from '../../redux/actions/event';
 import { getAllProductsShop } from '../../redux/actions/product';
+import { backend_url } from '../../server';
 import styles from '../../styles/styles';
+import Ratings from '../Products/Ratings';
 import ProductCard from '../Route/ProductCard/ProductCard';
 
 const ShopProfileData = ({ isOwner }) => {
   const { products } = useSelector((state) => state.products);
+  const { events } = useSelector((state) => state.events);
+  const { seller } = useSelector((state) => state.seller);
   const { id } = useParams();
   const dispatch = useDispatch();
   const [active, setActive] = useState(1);
 
   useEffect(() => {
     dispatch(getAllProductsShop(id));
+    dispatch(getAllEventsShop(seller._id));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch]);
+
+  const allReviews =
+    products && products.map((product) => product.reviews).flat();
 
   return (
     <div className="w-full">
@@ -63,16 +72,63 @@ const ShopProfileData = ({ isOwner }) => {
       </div>
 
       <br />
-      <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
-        {products &&
-          products.map((i, index) => (
-            <ProductCard data={i} key={index} isShop={true} />
-          ))}
-      </div>
-      {products && products.length === 0 && (
-        <h5 className="w-full text-center py-5 text-[18px]">
-          No Products have for this shop!
-        </h5>
+
+      {active === 1 && (
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
+          {products &&
+            products.map((i, index) => (
+              <ProductCard data={i} key={index} isShop={true} />
+            ))}
+        </div>
+      )}
+
+      {active === 2 && (
+        <div className="w-full">
+          <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
+            {events &&
+              events.map((i, index) => (
+                <ProductCard
+                  data={i}
+                  key={index}
+                  isShop={true}
+                  isEvent={true}
+                />
+              ))}
+          </div>
+          {events && events.length === 0 && (
+            <h5 className="w-full text-center py-5 text-[18px]">
+              No Events have for this shop!
+            </h5>
+          )}
+        </div>
+      )}
+
+      {active === 3 && (
+        <div className="w-full">
+          {allReviews &&
+            allReviews.map((item, index) => (
+              <div className="w-full flex my-4" key={index}>
+                <img
+                  src={`${backend_url}/${item.user.avatar}`}
+                  className="w-[50px] h-[50px] rounded-full"
+                  alt=""
+                />
+                <div className="pl-2">
+                  <div className="flex w-full items-center">
+                    <h1 className="font-[600] pr-2">{item.user.name}</h1>
+                    <Ratings rating={item.rating} />
+                  </div>
+                  <p className="font-[400] text-[#000000a7]">{item?.comment}</p>
+                  <p className="text-[#000000a7] text-[14px]">{'2days ago'}</p>
+                </div>
+              </div>
+            ))}
+          {allReviews && allReviews.length === 0 && (
+            <h5 className="w-full text-center py-5 text-[18px]">
+              No Reviews have for this shop!
+            </h5>
+          )}
+        </div>
       )}
     </div>
   );
