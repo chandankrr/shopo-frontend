1d6e8df90806ebfbaed5c19bfca6370de96d7897
Chandan Kumar <chandankrr.91@gmail.com>
2023-12-03 22:44:01 +0530

    added tagline in hero section

--- src/components/Route/Hero/Hero.jsx
+++ src/components/Route/Hero/Hero.jsx
@@ -15,13 +15,12 @@ const Hero = () => {
         <h1
           className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
         >
-          Best Collection for <br /> Home Decoration
+          Welcome to ShopO – Where Fashion is a Journey, Not a Destination!
         </h1>
         <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
-          assumenda? Quisquam itaque <br /> exercitationem labore vel, dolore
-          quidem asperiores, laudantium temporibus soluta optio consequatur{' '}
-          <br /> aliquam deserunt officia. Dolorum saepe nulla provident.
+          Dive into a world of limitless possibilities and redefine your
+          wardrobe with ShopO, your ultimate <br /> destination for curated
+          fashion that doesn't compromise on your budget.
         </p>
         <Link to="/products" className="inline-block">
           <div className={`${styles.button} mt-5`}>
--- src/static/data.js
+++ src/static/data.js
@@ -195,69 +195,70 @@ export const categoriesData = [
     title: 'Computers and Laptops',
     subTitle: '',
     image_Url:
-      'https://cdn.shopify.com/s/files/1/1706/9177/products/NEWAppleMacbookProwithM1ProChip14InchLaptop2021ModelMKGQ3LL_A_16GB_1TBSSD_custommacbd.jpg?v=1659592838',
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-01.jpg',
   },
   {
     id: 2,
     title: 'Cosmetics and Body Care',
     subTitle: '',
     image_Url:
-      'https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-07/kosme1.png',
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-02.jpg',
   },
   {
     id: 3,
     title: 'Accesories',
     subTitle: '',
     image_Url:
-      'https://img.freepik.com/free-vector/ordering-goods-online-internet-store-online-shopping-niche-e-commerce-website-mother-buying-babies-clothes-footwear-toys-infant-accessories_335657-2345.jpg?w=2000',
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-03.jpg',
   },
   {
     id: 4,
     title: 'Cloths',
     subTitle: '',
     image_Url:
-      'https://www.shift4shop.com/2015/images/industries/clothing/clothing-apparel.png',
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-04.jpg',
   },
   {
     id: 5,
     title: 'Shoes',
     subTitle: '',
     image_Url:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvBQPQMVNRd6TtDkGs2dCri0Y-rxKkFOiEWw&usqp=CAU',
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-05.jpg',
   },
   {
     id: 6,
     title: 'Gifts',
     subTitle: '',
     image_Url:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvBQPQMVNRd6TtDkGs2dCri0Y-rxKkFOiEWw&usqp=CAU',
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-06.jpg',
   },
   {
     id: 7,
     title: 'Pet Care',
     subTitle: '',
-    image_Url: 'https://cdn.openpr.com/T/c/Tc15444071_g.jpg',
+    image_Url:
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-07.jpg',
   },
   {
     id: 8,
     title: 'Mobile and Tablets',
     subTitle: '',
     image_Url:
-      'https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mpwp3tua-apple-iphone-14-256gb-mavi-mpwp3tua-637986832343472449.jpg',
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-08.jpg',
   },
   {
     id: 9,
     title: 'Music and Gaming',
     subTitle: '',
     image_Url:
-      'https://static.vecteezy.com/system/resources/previews/011/996/555/original/3d-black-headphone-illustration-ecommerce-icon-png.png',
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-09.jpg',
   },
   {
     id: 10,
     title: 'Others',
     subTitle: '',
     image_Url:
-      'https://searchspring.com/wp-content/uploads/2022/10/Hero-Image-Platform-Others-2.png',
+      'https://raw.githubusercontent.com/chandankrr/shopo-frontend/main/src/Assests/images/ct-10.jpg',
   },
 ];
 
